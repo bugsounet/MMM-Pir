69b49611637d89cc83ce57aad1a11710755f2f41
bugsounet <bugsounet@bugsounet.fr>
2023-03-28 18:18:05 +0200

    fix not needed css (already loaded)

--- EXT-Screen.js
+++ EXT-Screen.js
@@ -199,10 +199,7 @@ Module.register("EXT-Screen", {
     },
 
     getStyles: function () {
-      return [
-        "EXT-Screen.css",
-        "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
-      ]
+      return [ "EXT-Screen.css" ]
     },
 
     getScripts: function () {
