ab7ed4b508ae5b2756c55adcb3157b0dc22238de
bugsounet <github@bugsounet.fr>
2024-02-17 14:47:37 +0100

    eslint fix (screenTouch.js)

--- components/screenTouch.js
+++ components/screenTouch.js
@@ -2,76 +2,82 @@
 * screenTouch *
 * Bugsounet   *
 ***************/
+
+/* global window */
+
 class screenTouch {
-  constructor(mode,Tools) {
-    this.mode = mode
-    this.sendSocketNotification = (...args) => Tools.sendSocketNotification(...args)
-    this.hidden = () => Tools.hidden()
-    if (this.mode > 3 || this.mode < 0 || isNaN(this.mode)) this.mode = 3
-    this.clickTimer = null
-    this.clickCount = 0
-    console.log("[MMM-Pir] screenTouch Ready")
+  constructor (mode, Tools) {
+    this.mode = mode;
+    this.sendSocketNotification = (...args) => Tools.sendSocketNotification(...args);
+    this.hidden = () => Tools.hidden();
+    if (this.mode > 3 || this.mode < 0 || isNaN(this.mode)) this.mode = 3;
+    this.clickTimer = null;
+    this.clickCount = 0;
+    console.log("[MMM-Pir] screenTouch Ready");
   }
 
-  touch() {
-    let TouchScreen = document.getElementById("MMM-PIR")
+  touch () {
+    let TouchScreen = document.getElementById("MMM-PIR");
 
     switch (this.mode) {
       case 1:
-      /** mode 1 **/
-        window.addEventListener('click', () => {
-          this.clickCount++
+
+        /** mode 1 **/
+        window.addEventListener("click", () => {
+          this.clickCount++;
           if (this.clickCount === 1) {
             this.clickTimer = setTimeout(() => {
-              this.clickCount = 0
-              this.sendSocketNotification("LOCK_FORCE_WAKEUP")
-            }, 400)
+              this.clickCount = 0;
+              this.sendSocketNotification("LOCK_FORCE_WAKEUP");
+            }, 400);
           } else if (this.clickCount === 2) {
-            clearTimeout(this.clickTimer)
-            this.clickCount = 0
-            this.sendSocketNotification("LOCK_FORCE_END")
+            clearTimeout(this.clickTimer);
+            this.clickCount = 0;
+            this.sendSocketNotification("LOCK_FORCE_END");
           }
-        }, false)
-        break
+        }, false);
+        break;
       case 2:
-      /** mode 2 **/
-        TouchScreen.addEventListener('click', () => {
-          if (!this.hidden()) this.sendSocketNotification("LOCK_FORCE_WAKEUP")
-        }, false)
 
-        window.addEventListener('long-press', () => {
-          if (this.hidden()) this.sendSocketNotification("LOCK_FORCE_WAKEUP")
+        /** mode 2 **/
+        TouchScreen.addEventListener("click", () => {
+          if (!this.hidden()) this.sendSocketNotification("LOCK_FORCE_WAKEUP");
+        }, false);
+
+        window.addEventListener("long-press", () => {
+          if (this.hidden()) this.sendSocketNotification("LOCK_FORCE_WAKEUP");
           else {
-            this.sendSocketNotification("LOCK_FORCE_END")
+            this.sendSocketNotification("LOCK_FORCE_END");
           }
-        }, false)
-        break
+        }, false);
+        break;
       case 3:
-      /** mode 3 **/
-        TouchScreen.addEventListener('click', () => {
-          this.clickCount++
+
+        /** mode 3 **/
+        TouchScreen.addEventListener("click", () => {
+          this.clickCount++;
           if (this.clickCount === 1) {
             this.clickTimer = setTimeout(() => {
-              this.clickCount = 0
-              this.sendSocketNotification("LOCK_FORCE_WAKEUP")
-            }, 400)
+              this.clickCount = 0;
+              this.sendSocketNotification("LOCK_FORCE_WAKEUP");
+            }, 400);
           } else if (this.clickCount === 2) {
-            clearTimeout(this.clickTimer)
-            this.clickCount = 0
-            this.sendSocketNotification("LOCK_FORCE_END")
+            clearTimeout(this.clickTimer);
+            this.clickCount = 0;
+            this.sendSocketNotification("LOCK_FORCE_END");
           }
-        }, false)
+        }, false);
 
-        window.addEventListener('click', () => {
+        window.addEventListener("click", () => {
           if (this.hidden()) {
-            clearTimeout(this.clickTimer)
-            this.clickCount = 0
-            this.sendSocketNotification("LOCK_FORCE_WAKEUP")
+            clearTimeout(this.clickTimer);
+            this.clickCount = 0;
+            this.sendSocketNotification("LOCK_FORCE_WAKEUP");
           }
-        }, false)
-        break
+        }, false);
+        break;
     }
-    if (!this.mode) console.log("[MMM-Pir] Touch Screen Function disabled.")
-    else console.log(`[MMM-Pir] Touch Screen Function added. [mode ${this.mode}]`)
+    if (!this.mode) console.log("[MMM-Pir] Touch Screen Function disabled.");
+    else console.log(`[MMM-Pir] Touch Screen Function added. [mode ${this.mode}]`);
   }
 }
