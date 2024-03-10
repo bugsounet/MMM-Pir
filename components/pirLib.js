ae142efce0ad4b4445c10d97b50a490b371be7d0
bugsounet <bugsounet@bugsounet.fr>
2024-03-10 01:46:51 +0100

    correct logs

--- components/pirLib.js
+++ components/pirLib.js
@@ -53,54 +53,12 @@ class PIR {
     log("Stop");
   }
 
-  gpiozeroDetect () {
-    const { PythonShell } = require("python-shell");
-    let options = {
-      mode: "text",
-      scriptPath: __dirname,
-      pythonOptions: ["-u"],
-      args: [ "-g", this.config.gpio ]
-    };
-
-    this.pir = new PythonShell("MotionSensor.py", options);
-    this.callback("PIR_STARTED");
-    console.log("[MMM-Pir] [LIB] [PIR] Started in MODE 2!");
-    this.running = true;
-
-    this.pir.on("message", (message) => {
-      // detect pir
-      if (message === "Detected") {
-        log("Detected presence");
-        this.callback("PIR_DETECTED");
-      } else {
-        console.error("[MMM-Pir] [LIB] [PIR] ", message);
-        this.callback("PIR_ERROR", message);
-        this.running = false;
-      }
-    });
-
-    this.pir.on("stderr", (stderr) => {
-      // handle stderr (a line of text from stderr)
-      if (this.config.debug) console.error("[MMM-Pir] [LIB] [PIR]", stderr);
-      this.running = false;
-    });
-
-    this.pir.end((err,code,signal) => {
-      if (err) {
-        console.error("[MMM-Pir] [LIB] [PIR] [PYTHON]",err);
-        this.callback("PIR_ERROR", err.message);
-      }
-      console.warn(`[MMM-Pir] [LIB] [PIR] [PYTHON] The exit code was: ${code}`);
-      console.warn(`[MMM-Pir] [LIB] [PIR] [PYTHON] The exit signal was: ${signal}`);
-    });
-  }
-
   onoffDetect () {
     try {
       const Gpio = require("onoff").Gpio;
       this.pir = new Gpio(this.config.gpio, "in", "both");
       this.callback("PIR_STARTED");
-      console.log("[MMM-Pir] [LIB] [PIR] Started in MODE 0!");
+      console.log("[MMM-Pir] [LIB] [PIR] Started!");
     } catch (err) {
       console.error(`[MMM-Pir] [LIB] [PIR] ${err}`);
       this.running = false;
@@ -131,7 +89,7 @@ class PIR {
 
     this.pir = new PythonShell("gpioSensor.py", options);
     this.callback("PIR_STARTED");
-    console.log("[MMM-Pir] [LIB] [PIR] Started in MODE 1!");
+    console.log("[MMM-Pir] [LIB] [PIR] Started!");
     this.running = true;
 
     this.pir.on("message", (message) => {
@@ -141,6 +99,48 @@ class PIR {
         this.callback("PIR_DETECTED");
       } else {
         console.error("[MMM-Pir] [LIB] [PIR]", message);
+        this.callback("PIR_ERROR", message);
+        this.running = false;
+      }
+    });
+
+    this.pir.on("stderr", (stderr) => {
+      // handle stderr (a line of text from stderr)
+      if (this.config.debug) console.error("[MMM-Pir] [LIB] [PIR]", stderr);
+      this.running = false;
+    });
+
+    this.pir.end((err,code,signal) => {
+      if (err) {
+        console.error("[MMM-Pir] [LIB] [PIR] [PYTHON]",err);
+        this.callback("PIR_ERROR", err.message);
+      }
+      console.warn(`[MMM-Pir] [LIB] [PIR] [PYTHON] The exit code was: ${code}`);
+      console.warn(`[MMM-Pir] [LIB] [PIR] [PYTHON] The exit signal was: ${signal}`);
+    });
+  }
+
+  gpiozeroDetect () {
+    const { PythonShell } = require("python-shell");
+    let options = {
+      mode: "text",
+      scriptPath: __dirname,
+      pythonOptions: ["-u"],
+      args: [ "-g", this.config.gpio ]
+    };
+
+    this.pir = new PythonShell("MotionSensor.py", options);
+    this.callback("PIR_STARTED");
+    console.log("[MMM-Pir] [LIB] [PIR] Started!");
+    this.running = true;
+
+    this.pir.on("message", (message) => {
+      // detect pir
+      if (message === "Detected") {
+        log("Detected presence");
+        this.callback("PIR_DETECTED");
+      } else {
+        console.error("[MMM-Pir] [LIB] [PIR] ", message);
         this.callback("PIR_ERROR", message);
         this.running = false;
       }
