90f975da8dda2e2ccf7f74e0f3bc92419d6d63a4
bugsounet <github@bugsounet.fr>
2024-08-30 01:01:49 +0200

    delete async

--- components/pirLib.js
+++ components/pirLib.js
@@ -177,7 +177,7 @@ class PIR {
 
   /* experimental */
 
-  async gpiodDetect () {
+  gpiodDetect () {
     try {
       const { version, Chip, Line } = require("node-libgpiod");
       const numbers = [0,1,2,3,4,5,6,7,8,9,10];
