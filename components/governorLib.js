e5b46747292c2a4551ec82860e2a1599938dbcce
bugsounet <github@bugsounet.fr>
2024-11-16 13:09:24 +0100

    lint: governorLib.js

--- components/governorLib.js
+++ components/governorLib.js
@@ -3,10 +3,10 @@
 
 const exec = require("child_process").exec;
 
-var log = (...args) => { /* do nothing */ };
+var log = () => { /* do nothing */ };
 
 class GOVERNOR {
-  constructor (config,callback) {
+  constructor (config, callback) {
     this.config = config;
     this.error = callback.error;
     this.default = {
@@ -16,11 +16,11 @@ class GOVERNOR {
     };
     this.config = Object.assign(this.default, this.config);
     if (this.config.debug === true) log = (...args) => { console.log("[MMM-Pir] [LIB] [GOVERNOR]", ...args); };
-    this.MyGovernor = [ "Disabled", "conservative", "ondemand" , "userspace" , "powersave" , "performance" ];
+    this.MyGovernor = ["Disabled", "conservative", "ondemand", "userspace", "powersave", "performance"];
     this.Governor = {
-      actived : false,
-      wanted : "",
-      actual : "",
+      actived: false,
+      wanted: "",
+      actual: "",
       error: null
     };
     console.log("[MMM-Pir] [LIB] [GOVERNOR] Governor library initialized...");
@@ -42,31 +42,31 @@ class GOVERNOR {
   }
 
   apply (type) {
-    exec("cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor", (error, stdout, stderr) => {
+    exec("cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor", (error, stdout) => {
       if (error) {
-        this.Governor.actived= false;
-        this.Governor.error= "Incompatible with your system.";
+        this.Governor.actived = false;
+        this.Governor.error = "Incompatible with your system.";
         console.error(`[MMM-Pir] [LIB] [GOVERNOR] ${type} - Error: ${this.Governor.error}`);
         this.error(this.Governor.error);
         return;
       }
-      this.Governor.actual = stdout.replace(/\n|\r|(\n\r)/g,"");
+      this.Governor.actual = stdout.replace(/\n|\r|(\n\r)/g, "");
       log(`Actual: ${this.Governor.actual}`);
       if (this.Governor.actual === this.Governor.wanted) {
-        this.Governor.error= null;
+        this.Governor.error = null;
         this.Governor.actived = true;
         log("Already set");
       } else {
         this.MyGovernor.forEach((governor) => {
           if (governor === this.Governor.wanted) {
             exec(`echo ${governor} | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor`);
-            this.Governor.error= null;
+            this.Governor.error = null;
             this.Governor.actived = true;
             log(`${type} Set: ${governor}`);
           }
         });
         if (!this.Governor.actived) {
-          this.Governor.error= `Unknow Governor (${this.Governor.wanted})`;
+          this.Governor.error = `Unknow Governor (${this.Governor.wanted})`;
           this.Governor.actived = false;
           console.error(`[MMM-Pir] [LIB] [GOVERNOR] ${type} ${this.Governor.error}`);
           this.error(this.Governor.error);
